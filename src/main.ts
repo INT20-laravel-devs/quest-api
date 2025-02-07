e5aabb6c636fff80a8d4efb6b9b6145451bc28f1
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2025-02-07 10:11:34 +0200

    feat: init nest-fastify project

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () =>
+    console.info(`Server is running on http://127.0.0.1:${port}`),
+  );
+}
+void bootstrap();
