548c974c8ebd5a7df843136567c5ac3ce0d03823
Illia <kroshka.illya@lll.kpi.ua>
2025-02-09 11:52:24 +0200

    chore: add email to link

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
       to,
       subject: 'Email verification on Questly.com',
       message: 'Please click the link below to verify your email address:',
-      link: `${baseUrl}/auth/approve-email/?token=${token}`,
+      link: `${baseUrl}/auth/approve-email/?token=${token}&email=${to}`,
     });
     return token;
   }
